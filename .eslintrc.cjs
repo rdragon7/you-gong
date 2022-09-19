95fdfe13521e32eeb7fe89d4aa874306c4f789ce
coder7 <ren760824@163.com>
2022-09-19 14:41:32 +0800

    🎉 init(init): 项目初始化

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: [
+    'standard',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+    // "plugin:prettier/recommended"
+  ],
+  overrides: [],
+  settings: {
+    react: {
+      version: '18.0'
+    }
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'space-before-function-paren': 0
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+function App() {
+  return <div className="App">app...</div>
+}
+
+export default App
